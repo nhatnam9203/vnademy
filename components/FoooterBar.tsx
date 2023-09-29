db908a7e239efa5fbdab625e9e208ca916dea657
Phan Nhật Nam <84962280+namphanlevinci@users.noreply.github.com>
2023-09-29 16:17:44 +0700

    .

--- components/FoooterBar.tsx
+++ components/FoooterBar.tsx
@@ -1,29 +1,18 @@
 import React from "react";
-import { logo_vnademy } from "assets";
-import { Container } from "react-bootstrap";
-import CustomImage from "./CustomImage";
 import CustomButton from "./CustomButton";
 import CustomText from "./CustomText";
-import CustomIcon from "./CustomIcon";
 import styles from './style.module.css';
-import {
-    ic_cart,
-    ic_lock,
-    ic_people,
-} from "assets/index";
-import SearchBar from "./SearchBar";
-import Stack from "react-bootstrap/Stack";
 
 export default function FoooterBar() {
 
     const register = () => {
 
     };
-
+""
     return (
         <div className={styles.footerbar_wrap}>
                 <CustomText
-                    title="Trở thành Giảng viên tại VNademy ngay hôm nay!"
+                    title="TRỞ THÀNH GIẢNG VIÊN TẠI VNADEMY NGAY HÔM NAY!"
                     color="white"
                     fontWeight={800}
                     fontSize={32}
