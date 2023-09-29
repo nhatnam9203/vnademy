26f22c0ae69fffde12cfd92be360e41ed8802537
Phan Nhật Nam <84962280+namphanlevinci@users.noreply.github.com>
2023-09-29 17:30:06 +0700

    add icon

--- assets/index.ts
+++ assets/index.ts
@@ -4,6 +4,11 @@ import ic_search from "./ic_search.png";
 import ic_people from "./ic_people.png";
 import ic_cart from "./ic_cart.png";
 import ic_lock from "./ic_lock.png";
+import ic_facebook from "./ic_facebook.png";
+import ic_google_rounded from "./ic_google_rounded.png";
+import ic_twitter from "./ic_twitter.png";
+import ic_youtube from "./ic_youtube.png";
+
 
 export {
     logo_vnademy,
@@ -11,5 +16,9 @@ export {
     ic_lock,
     ic_search,
     ic_cart,
-    ic_people
+    ic_people,
+    ic_facebook,
+    ic_google_rounded,
+    ic_twitter,
+    ic_youtube
 }
