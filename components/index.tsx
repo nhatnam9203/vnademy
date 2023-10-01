ca989fad0918832a03d9d770f1198ad5f448f34f
namphanlevinci <nam.phan@levincigroup.com>
2023-10-01 18:12:26 +0700

    update code

---
+++ components/index.tsx
@@ -0,0 +1,23 @@
+import CustomButton from "./CustomButton";
+import CustomForm from "./CustomForm";
+import CustomFormInput from "./CustomFormInput";
+import CustomIcon from "./CustomIcon";
+import CustomImage from "./CustomImage";
+import CustomInput from "./CustomInput";
+import CustomText from "./CustomText";
+import HeadPage from "./HeadPage";
+import SearchBar from "./SearchBar";
+import TopBar from "./TopBar";
+
+export {
+    CustomButton,
+    CustomFormInput,
+    CustomForm,
+    CustomIcon,
+    CustomImage,
+    CustomInput,
+    CustomText,
+    HeadPage,
+    SearchBar,
+    TopBar
+}
