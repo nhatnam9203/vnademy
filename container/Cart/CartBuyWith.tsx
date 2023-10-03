69108fffeda7ef0818279a2064c4783f9a58eeaa
namphanlevinci <nam.phan@levincigroup.com>
2023-10-03 22:21:28 +0700

    update code

--- container/Cart/CartBuyWith.tsx
+++ container/Cart/CartBuyWith.tsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import {
+    CustomText,
+    CustomButton
+} from "components";
 
 interface IProps {
 };
@@ -6,7 +11,15 @@ interface IProps {
 export default function CartBuyWith({
 }: IProps) {
     return (
-        <>
-        </>
+        <Container>
+            <Row style={{ paddingTop: 30 }}>
+                <CustomText
+                    title="THƯỜNG ĐƯỢC MUA CÙNG"
+                    color="#1D252C"
+                    fontWeight={700}
+                    fontSize={26}
+                />
+            </Row>
+        </Container>
     )
 }
