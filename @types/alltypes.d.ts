d62c87f0295ce7d96c25ed63753c6faf35eb40c0
namphanlevinci <nam.phan@levincigroup.com>
2023-10-10 13:09:28 +0700

    update code

--- @types/alltypes.d.ts
+++ @types/alltypes.d.ts
@@ -1,2 +1,3 @@
 declare module 'react-outside-click-handler';
 declare module 'react-slick';
+declare module 'react-slide-toggle';
--- container/CourseDetail/CourseContent.tsx
+++ container/CourseDetail/CourseContent.tsx
@@ -13,6 +13,7 @@ import {
 } from "assets";
 import Image from 'next/image';
 import { Stack } from 'react-bootstrap';
+import SlideToggle from "react-slide-toggle";
 
 export default function () {
     return (
@@ -26,88 +27,121 @@ export default function () {
                 fontSize={20}
                 fontWeight={700}
             />
-
-
-            <CustomRow
-                style={{
-                    background: "#F6FAFF",
-                    border: "1px solid #C5CBD5",
-                    cursor: "pointer"
-                }}>
-                <div className={styles.icon_plus}>
-                    <Image
-                        src={ic_plus_black}
-                        width={16}
-                        height={16}
-                        alt='ic_'
-                    />
-                </div>
-                <CustomText
-                    title='Phần 1: Giới thiệu tổng quan và các thiết lập ban đầu'
-                    fontWeight={700}
-                    fontSize={14}
-                    color='#1D252C'
-                    style={{ marginLeft: 16 }}
-                />
-            </CustomRow>
-
-            {
-                new Array(4).fill(null).map(it => (
-
-                    <CustomRow
-                        key={Math.random()}
-                        style={{
-                            justifyContent: "space-between",
-                            marginTop: 16
-                        }} isBetween
-                    >
-                        <CustomRow>
-                            <Image
-                                src={ic_play}
-                                width={18}
-                                height={18}
-                                alt='ic_play'
-                                style={{ marginLeft: 8 }}
-                            />
-                            <CustomText
-                                title='Bài 1: Giới thiệu và các thiết lập cơ bản'
-                                color='#1D252C'
-                                fontWeight={600}
-                                fontSize={14}
-                                style={{ marginLeft: 8 }}
-                            />
-                        </CustomRow>
-
-                        <CustomRow style={{ justifyContent: "flex-end" }}>
-                            <div>
-
-                            </div>
-
-                            <CustomButton
-                                title='Học thử'
-                                onClick={() => { }}
-                                height={30}
-                                width={80}
-                                fontWeight={600}
-                                style={{
-                                    background: "transparent",
-                                    color: "#2B5DF5",
-                                    border: "1px solid #2B5DF5",
-                                    fontSize: 14
-                                }}
-                            />
-                            <CustomText
-                                title='08:05'
-                                color='#55555A'
-                                fontWeight={600}
-                                fontSize={14}
-                                style={{ width: 120, textAlign: "right" }}
-                            />
-                        </CustomRow>
-                    </CustomRow>
-
-                ))
-            }
+            <div style={{ marginTop: 20 }}>
+            <CourseItem />
+            <CourseItem />
+            <CourseItem />
+            </div>
         </div>
     );
-}
+};
+
+const CourseItem = () => {
+
+    const [isCollapsed, setCollapsed] = React.useState<boolean>(false);
+
+    return (
+        <SlideToggle
+            onExpanding={() => setCollapsed(true)}
+            onCollapsed={() => setCollapsed(false)}
+            render={({
+                toggle, setCollapsibleElement }: any) => (
+                <div className="my-collapsible">
+                    <div onClick={toggle} className="my-collapsible__toggle">
+                        <CustomRow
+                            style={{
+                                background: "#F6FAFF",
+                                border: "1px solid #C5CBD5",
+                                cursor: "pointer"
+                            }}>
+                            <div className={styles.icon_plus}>
+                                {
+                                    !isCollapsed ?
+                                        <Image
+                                            src={ic_minus}
+                                            width={16}
+                                            height={2}
+                                            alt='ic_'
+                                        /> : <Image
+                                            src={ic_plus_black}
+                                            width={16}
+                                            height={16}
+                                            alt='ic_'
+                                        />
+                                }
+                            </div>
+                            <CustomText
+                                title='Phần 1: Giới thiệu tổng quan và các thiết lập ban đầu'
+                                fontWeight={700}
+                                fontSize={14}
+                                color='#1D252C'
+                                style={{ marginLeft: 16 }}
+                            />
+                        </CustomRow>
+                    </div>
+                    <div className="my-collapsible__content" ref={setCollapsibleElement}>
+                        <div className="my-collapsible__content-inner">
+                            {
+                                new Array(4).fill(null).map(it => (
+
+                                    <CustomRow
+                                        key={Math.random()}
+                                        style={{
+                                            justifyContent: "space-between",
+                                            marginTop: 16
+                                        }} isBetween
+                                    >
+                                        <CustomRow>
+                                            <div style={{ borderRight: "none" }} className={styles.icon_plus}>
+                                                <Image
+                                                    src={ic_play}
+                                                    width={18}
+                                                    height={18}
+                                                    alt='ic_play'
+                                                />
+                                            </div>
+                                            <CustomText
+                                                title='Bài 1: Giới thiệu và các thiết lập cơ bản'
+                                                color='#1D252C'
+                                                fontWeight={600}
+                                                fontSize={14}
+                                                style={{ marginLeft: 16 }}
+                                            />
+                                        </CustomRow>
+
+                                        <CustomRow style={{ justifyContent: "flex-end" }}>
+                                            <div>
+
+                                            </div>
+
+                                            <CustomButton
+                                                title='Học thử'
+                                                onClick={() => { }}
+                                                height={30}
+                                                width={80}
+                                                fontWeight={600}
+                                                style={{
+                                                    background: "transparent",
+                                                    color: "#2B5DF5",
+                                                    border: "1px solid #2B5DF5",
+                                                    fontSize: 14
+                                                }}
+                                            />
+                                            <CustomText
+                                                title='08:05'
+                                                color='#55555A'
+                                                fontWeight={600}
+                                                fontSize={14}
+                                                style={{ width: 120, textAlign: "right" }}
+                                            />
+                                        </CustomRow>
+                                    </CustomRow>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            )}
+        />
+    );
+};
